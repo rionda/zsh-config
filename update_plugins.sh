300873169e22a8cb11ee110c3c6840f0e711c340
Matteo Riondato <rionda@acm.org>
2021-12-21 10:46:56 -0500

    Handle using rebase as the default strategy in our global gitconfig, and terminate early in case of failure.

--- update_plugins.sh
+++ update_plugins.sh
@@ -1,11 +1,9 @@
 #! /bin/sh
-echo "## Pulling from remote" && \
-git pull && \
-echo "## Syncing submodules " &&  \
-git submodule sync && \
-echo "## Updating/Initing and merging submodules from their remote" &&  \
-git submodule update --init --remote --merge && \
-echo "## Committing update " &&  \
-git commit --no-gpg-sign plugins -m "Sync submodules." && \
-echo "## Pushing " &&  \
-git push
+(echo "## Pulling from remote" &&  git pull --no-rebase) || exit 1
+(echo "## Syncing submodules " && git submodule sync) || exit 1
+(echo "## Updating/Initing and merging submodules from their remote" &&  \
+    git submodule update --init --remote --merge) || exit 1
+(echo "## Committing update " &&  \
+    git commit --no-gpg-sign plugins -m "Sync submodules.") || \
+    exit 1
+(echo "## Pushing " && git push) || exit 1
