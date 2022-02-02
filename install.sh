5878eb839638cf656f3f669aaccc5bf73042fda5
Matteo Riondato <rionda@acm.org>
2022-02-02 07:43:37 -0500

    Init submodule recursively, just in case.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-git submodule update --init --merge && \
+git submodule update --init --merge --recursive && \
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
     ln -s "${PWD}/zshrc" ~/.zshrc
